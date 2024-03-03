08d96ab17cd253b68b89ad0b4070b7d6ed364938
Jihye Bae <Jhbae1@sheffield.ac.uk>
2024-03-03 15:40:42 +0000

    Add users table

--- db/migrate/20240227131058_create_users.rb
+++ db/migrate/20240227131058_create_users.rb
@@ -1,8 +1,19 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+      t.string :user_name
+      t.string :email
+      t.string :password
+      t.integer :role     # 1-guest, 2-student, 3-lab_personnel, 4-admin
+      t.text :training_status
+      # t.text : special_permission
+      t.date :last_sign_up
+      t.text :personal_materials
+      t.text :personal_builds
 
       t.timestamps
     end
+    add_foreign_key :users, :materials, column: :personal_materials
+    add_foreign_key :users, :builds, column: :personal_builds
   end
 end
