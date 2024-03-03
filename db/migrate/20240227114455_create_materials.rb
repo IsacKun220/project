00cd020a3fd3068b0fbcce1b27a332dbb0339ec7
Yuet Him Isaac Ho <yho5@sheffield.ac.uk>
2024-03-03 17:50:50 +0000

    Adding the materials table

--- db/migrate/20240227114455_create_materials.rb
+++ db/migrate/20240227114455_create_materials.rb
@@ -1,7 +1,17 @@
 class CreateMaterials < ActiveRecord::Migration[7.0]
   def change
     create_table :materials do |t|
-
+     t.text :material_name,  :null => false
+     t.text :attachments_texts
+     t.decimal :melting_point, precision: 4, scale: 1
+     t.decimal :solid_point, precision: 4, scale: 1
+     t.integer :quantity
+     t.text :location
+     t.date :last_stocked
+     t.integer :privacy_level
+     t.decimal :tensile_strength, precision: 4, scale:2
+     t.text :flow_additive
+     t.string :availability
       t.timestamps
     end
   end
